91f1926282326556c93fc389b5a35f06723d0392
sufiyan <sufiyanch706@gmail.com>
2023-10-26 17:09:42 +0530

    launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => const HomePage(),
         '/add': (context) => const AddUser(),
       },
-      initialRoute: '/',
+      initialRoute: '/splash',
     );
   }
 }
